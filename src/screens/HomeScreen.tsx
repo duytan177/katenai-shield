ff176ad27c0b5f15aa37cb2df1471f8cb571e45f
hggnomm <hggnomm.dev@gmail.com>
2024-12-15 16:31:56 +0700

    commit

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -4,57 +4,107 @@ import {
   Text,
   Pressable,
   Image,
+  FlatList,
   TouchableOpacity,
+  ScrollView,
 } from "react-native";
 import React from "react";
 import Header from "../components/Header";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { LinearGradient } from "expo-linear-gradient";
 import speakerImg from "../assets/images/HomeView/speake-view.png";
+import anocall from "../assets/images/HomeView/anocall.png";
+import blogs from "../assets/images/HomeView/blogs.png";
+import fakecall from "../assets/images/HomeView/fakecall.png";
+import house from "../assets/images/HomeView/house.png";
+import record from "../assets/images/HomeView/record.png";
+import safetips from "../assets/images/HomeView/safetips.png";
+import sos from "../assets/images/HomeView/sos.png";
+import trackme from "../assets/images/HomeView/trackme.png";
+
+const FeatureItem = ({ title, imageSource }: any) => {
+  return (
+    <TouchableOpacity style={styles.featureItem}>
+      <Image source={imageSource} style={styles.itemImage} />
+      <Text style={styles.itemTitle}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
 
 const HomeScreen = () => {
   const handleViewDetails = () => {
     console.log("Navigating to details...");
   };
 
+  const data = [
+    { id: "1", title: "Track me", imageSource: trackme },
+    { id: "2", title: "House of Compassion", imageSource: house },
+    { id: "3", title: "Fakecall", imageSource: fakecall },
+    { id: "4", title: "SOS", imageSource: sos },
+    { id: "5", title: "Safe Tips", imageSource: safetips },
+    { id: "6", title: "Records", imageSource: record },
+    { id: "7", title: "Blogs", imageSource: blogs },
+    { id: "8", title: "Anonymous Call", imageSource: anocall },
+  ];
+
+  const renderItem = ({ item }: any): any => {
+    return <FeatureItem title={item.title} imageSource={item.imageSource} />;
+  };
+
   return (
     <SafeAreaView>
       <Header />
-      <View style={styles.cardContainer}>
-        <LinearGradient
-          start={{ x: 0.5, y: 2.2 }}
-          end={{ x: 0.9, y: 1 }}
-          colors={["#88B8CC", "#445C66"]}
-          style={styles.cardSpeaker}
-        >
-          <View style={styles.container}>
-            <View style={styles.ovalBackground}></View>
-            <Image source={speakerImg} style={styles.speakerImage} />
-            <View style={styles.textContainer}>
-              <Text style={styles.textFirst}>
-                Có một sự kiện của một diễn giả có thể bạn quan tâm
-              </Text>
-              <TouchableOpacity
-                onPress={handleViewDetails}
-                style={styles.button}
-                activeOpacity={0.7}
-              >
-                <Text style={styles.buttonText}>View detail</Text>
-              </TouchableOpacity>
-              <TouchableOpacity>
-                <Text style={styles.textPrevious}>
-                  Look back at previous events.
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <View style={styles.cardContainer}>
+          <LinearGradient
+            start={{ x: 0.5, y: 2.2 }}
+            end={{ x: 0.9, y: 1 }}
+            colors={["#88B8CC", "#445C66"]}
+            style={styles.cardSpeaker}
+          >
+            <View style={styles.container}>
+              <View style={styles.ovalBackground}></View>
+              <Image source={speakerImg} style={styles.speakerImage} />
+              <View style={styles.textContainer}>
+                <Text style={styles.textFirst}>
+                  Có một sự kiện của một diễn giả có thể bạn quan tâm
                 </Text>
-              </TouchableOpacity>
+                <TouchableOpacity
+                  onPress={handleViewDetails}
+                  style={styles.button}
+                  activeOpacity={0.7}
+                >
+                  <Text style={styles.buttonText}>View detail</Text>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                  <Text style={styles.textPrevious}>
+                    Look back at previous events.
+                  </Text>
+                </TouchableOpacity>
+              </View>
             </View>
-          </View>
-        </LinearGradient>
-      </View>
+          </LinearGradient>
+        </View>
+
+        {/* FlatList for Feature Items */}
+        <View style={styles.featureCard}>
+          <FlatList
+            data={data}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.id}
+            numColumns={2} // Mỗi hàng có 2 item
+            contentContainerStyle={styles.featureCardContent}
+          />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  scrollContainer: {
+    flexGrow: 1, // Đảm bảo ScrollView sẽ cuộn khi cần
+  },
   cardContainer: {
     margin: 20,
   },
@@ -93,6 +143,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     marginRight: 15,
+    marginBottom: 5,
     justifyContent: "center",
     minHeight: "90%",
   },
@@ -113,7 +164,7 @@ const styles = StyleSheet.create({
     borderRadius: 30,
     paddingVertical: 10,
     paddingHorizontal: 20,
-    marginVertical: 10,
+    marginVertical: 20,
     alignItems: "center",
     justifyContent: "center",
   },
@@ -121,6 +172,29 @@ const styles = StyleSheet.create({
     color: "#445C66",
     fontSize: 12,
     fontWeight: "bold",
+  },
+  featureCard: {
+    marginTop: 20,
+    paddingHorizontal: 20,
+  },
+  featureCardContent: {
+    justifyContent: "space-between",
+  },
+  featureItem: {
+    width: "48%", // Mỗi item chiếm 48% chiều rộng
+    marginBottom: 15,
+    alignItems: "center",
+  },
+  itemImage: {
+    width: "100%",
+    height: 100,
+    resizeMode: "contain",
+  },
+  itemTitle: {
+    marginTop: 5,
+    fontSize: 14,
+    fontWeight: "600",
+    textAlign: "center",
   },
 });
 
