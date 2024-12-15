35c20018eaf5f60371ef0b02fc7c9c0cfe069927
hggnomm <hggnomm.dev@gmail.com>
2024-12-15 15:05:37 +0700

    home view

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,21 +1,72 @@
-import { StyleSheet, View, Text, Pressable } from "react-native";
+import { StyleSheet, View, Text, Pressable, Image } from "react-native";
 import React from "react";
 import Header from "../components/Header";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { LinearGradient } from "expo-linear-gradient";
+import speakerImg from "../assets/images/HomeView/speake-view.png";
 
 const HomeScreen = () => {
+  const handleViewDetails = () => {
+    console.log("Navigating to details...");
+  };
+
   return (
     <SafeAreaView>
       <Header />
-      <View>
-        <Pressable>
-          <Text>Homescreen</Text>
-        </Pressable>
+      <View style={styles.cardContainer}>
+        <LinearGradient
+          start={{ x: 0.5, y: 2.2 }}
+          end={{ x: 0.9, y: 1 }}
+          colors={["#88B8CC", "#445C66"]}
+          style={styles.cardSpeaker}
+        >
+          <View style={styles.container}>
+            <Image source={speakerImg} style={styles.speakerImage} />
+            <View style={styles.textContainer}>
+              <Text style={styles.text}>Homescreen</Text>
+              <Pressable onPress={handleViewDetails}>
+                <Text style={styles.text}>View detail</Text>
+              </Pressable>
+              <Text style={styles.text}>Look back at previous events.</Text>
+            </View>
+          </View>
+        </LinearGradient>
       </View>
     </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  cardContainer: {
+    margin: 20,
+  },
+  cardSpeaker: {
+    width: "100%",
+    height: 155,
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 10,
+  },
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    width: "100%",
+  },
+  speakerImage: {
+    width: 100,
+    height: "100%", // Make image take full height of its container
+    resizeMode: "contain", // Maintain aspect ratio
+    marginRight: 10,
+  },
+  textContainer: {
+    flex: 1,
+  },
+  text: {
+    color: "white",
+    marginBottom: 5,
+  },
+});
 
 export default HomeScreen;
